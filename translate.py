4b223987cae7fb24c1ed8bdc341c50ee257e6252
fourcolor <james1qaz2wsx12qw@gmail.com>
2022-01-01 22:54:54 +0800

    Translate command

--- translate.py
+++ translate.py
@@ -5,11 +5,14 @@ class Translater:
     def __init__(self) -> None:
         self.translater = googletrans.Translator()
         self.ans = ""
+        self.lan = ""
         pass
     def trans(self, msg, dst):
         if(dst in googletrans.LANGCODES.values()):
+            self.lan = dst
             self.ans = self.translater.translate(msg, dest=dst).text
         else:
+            self.lan = 'zh-tw'
             self.ans = self.translater.translate(msg, dest='zh-tw').text
         return self.ans
 
@@ -17,9 +20,8 @@ class Translater:
         return str(googletrans.LANGCODES)[1:-1].replace(',','\n').replace('\'','')
 
     def voice(self):
-        return gTTS(self.ans)
+        return gTTS(self.ans,lang = self.lan)
 
 if __name__ == "__main__":
     t = Translater()
-    print(t.trans("你好",""))
-    t.voice().save('static/'+'aslkdjlsakdjasij'+'.m4a')
+    print(t.trans("邂逅","en"))
