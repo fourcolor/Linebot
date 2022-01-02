abae4ddaa787fda651f735dbfc8bf4f91680a3c6
fourcolor <james1qaz2wsx12qw@gmail.com>
2022-01-02 22:26:45 +0800

    Finish README.md

--- fsm.py
+++ fsm.py
@@ -3,26 +3,31 @@ from transitions.extensions import *
 class FSMachine(object):
     def __init__(self) -> None:
         super().__init__()
-        self.states = ['Lobby','Translator','Chat bot','Pairing','Waiting','Paired']
+        self.states = ['Lobby','Translator','WithVoice','Chat bot','Pairing','Waiting','Paired']
         self.transitions = [
-            { 'trigger': 'PressTranslatorButton', 'source': 'Lobby', 'dest': 'Translator' },
-            { 'trigger': '!cl [語言簡寫]->更改翻譯的語言', 'source': 'Translator', 'dest': 'Translator' },
-            { 'trigger': '!ls ->顯示語言列表', 'source': 'Translator', 'dest': 'Translator' },
-            { 'trigger': '!!cv 1/0 ->是否要語音', 'source': 'Translator', 'dest': 'Translator' },
+            { 'trigger': 'PressTranslatorButton', 'source': 'Lobby', 'dest': 'WithVoice','conditions':'Audio on' },
+            { 'trigger': 'PressTranslatorButton', 'source': 'Lobby', 'dest': 'Translator','conditions':'Audio off' },
+            { 'trigger': '!cl', 'source': 'Translator', 'dest': 'Translator' },
+            { 'trigger': '!ls', 'source': 'Translator', 'dest': 'Translator' },
+            { 'trigger': '!!cv 1', 'source': 'Translator', 'dest': 'WithVoice' },
+            { 'trigger': '!!cv 0 ', 'source': 'WithVoice', 'dest': 'Translator' },
             { 'trigger': 'Press Chat bot Button', 'source': 'Lobby', 'dest': 'Chat bot' },
-            { 'trigger': 'Press Pairing Button', 'source': 'Lobby', 'dest': 'Pairing' },
-            { 'trigger': '!find ->開始配對', 'source': 'Pairing', 'dest': 'Paired','conditions':'Some one else' },
-            { 'trigger': '!find ->開始配對', 'source': 'Pairing', 'dest': 'Waiting','conditions':'No one else' },
+            { 'trigger': 'Press Pairing Button', 'source': 'Lobby', 'dest': 'Pairing','conditions':'haven\'t start pair' },
+            { 'trigger': 'Press Pairing Button', 'source': 'Lobby', 'dest': 'Waiting','conditions':'No one else' },
+            { 'trigger': 'Press Pairing Button', 'source': 'Lobby', 'dest': 'Paired','conditions':'Had Paired' },
+            { 'trigger': '!find', 'source': 'Pairing', 'dest': 'Paired','conditions':'Some one else' },
+            { 'trigger': '!find', 'source': 'Pairing', 'dest': 'Waiting','conditions':'No one else' },
             { 'trigger': 'λ', 'source': 'Waiting', 'dest': 'Paired','conditions':'Some one else' },
-            { 'trigger': '!lobby ->回到選單', 'source': 'Paired', 'dest': 'Lobby' },
-            { 'trigger': '!lobby ->回到選單', 'source': 'Waiting', 'dest': 'Lobby' },
-            { 'trigger': '!lobby ->回到選單', 'source': 'Pairing', 'dest': 'Lobby' },
-            { 'trigger': '!lobby ->回到選單', 'source': 'Chat bot', 'dest': 'Lobby' },
-            { 'trigger': '!lobby ->回到選單', 'source': 'Translator', 'dest': 'Lobby' },
+            { 'trigger': '!lobby', 'source': 'Paired', 'dest': 'Lobby' },
+            { 'trigger': '!lobby', 'source': 'Waiting', 'dest': 'Lobby' },
+            { 'trigger': '!lobby', 'source': 'Pairing', 'dest': 'Lobby' },
+            { 'trigger': '!lobby', 'source': 'Chat bot', 'dest': 'Lobby' },
+            { 'trigger': '!lobby', 'source': 'Translator', 'dest': 'Lobby' },
+            { 'trigger': '!lobby', 'source': 'WithVoice', 'dest': 'Lobby' },
         ]
 
 if __name__ == "__main__" :
     mach = FSMachine()
-    machine = Machine(model=mach, states=mach.states, transitions=mach.transitions,initial=mach.states[0])
-    mach.get_graph().draw('statediagram.png', prog='dot')
+    machine = GraphMachine(model=mach, states=mach.states, transitions=mach.transitions,initial=mach.states[0],show_conditions=True)
+    mach.get_graph().draw('img/statediagram.png', prog='dot')
     
